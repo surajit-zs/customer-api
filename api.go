8f2537d9409ec217414a0afbb4b216f9336411da
surajit-zs <surajit.pore@zopsmart.com>
2022-01-18 15:55:53 +0530

    add func get by id delete by id update by id

--- api.go
+++ api.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -43,11 +44,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		get(w, r)
 	case http.MethodPost:
 		post(w, r)
+	case http.MethodDelete:
+		deleteById(w, r)
+	case http.MethodPut:
+		updateById(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 
 		return
 	}
+}
+
+func deleteById(w http.ResponseWriter, r *http.Request) {
+	//defer db.Close()
+	query := r.URL.Query().Get("id")
+	_, err := db.Query("delete from Customer where Id=?", query)
+
+	if err != nil {
+		log.Println(err)
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 // post reads the JSON body and inserts in the database
@@ -68,7 +84,7 @@ func post(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = db.Exec("INSERT INTO Customer (ID ,Name ,Phone Address) VALUES(?,?,?,?)", customer.ID, customer.Name, customer.Phone, customer.Address)
+	_, err = db.Exec("INSERT INTO Customer (ID,Name,Phone,Address) VALUES(?,?,?,?)", customer.ID, customer.Name, customer.Phone, customer.Address)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 
@@ -111,6 +127,22 @@ func get(w http.ResponseWriter, r *http.Request) {
 
 	}
 
+	query := r.URL.Query().Get("id")
+	if query != "" {
+		for i, c := range customers {
+			if strconv.Itoa(c.ID) == query {
+				resp, _ := json.Marshal(customers[i])
+				_, err = w.Write(resp)
+
+				return
+			}
+		}
+		w.WriteHeader(http.StatusNotFound)
+
+		return
+
+	}
+
 	resp, err := json.Marshal(customers)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -121,4 +153,31 @@ func get(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func updateById(w http.ResponseWriter, r *http.Request) {
+	var customer Customer
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	err = json.Unmarshal(body, &customer)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	query := r.URL.Query().Get("id")
+	_, err = db.Exec("update Customer set Name = ?, Phone=?, Address=? where ID=?", customer.Name, customer.Phone, customer.Address, query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
